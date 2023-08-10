73dcc57601dae28752c47217402978332189ccb7
AznivManukyan <aznivmanukyan94@gmail.com>
2023-08-10 14:14:54 +0400

    first commit

---
+++ exe1.js
@@ -0,0 +1,21 @@
+//Array join
+
+//const arr1 = ["Arman","Azniv","Luse"];
+//const str = arr1.join ("*")
+//console.log(str);
+
+
+//const arr1 = ["Arman","Azniv"];
+//const arr2 = ['Luse'];
+//const arr3 = arr1.concat(arr2);
+
+//console.log(arr3);
+
+//const arr3 = [...arr1,...arr2];//rest or speead
+//console.log(arr3);
+
+const Array = [10,20,30];
+Array[0] = (Array[0]+3);
+Array[1] = (Array[1]+3);
+Array[2] = (Array[2]+3);
+console.log(Array);
---
+++ exe2.js
@@ -0,0 +1,11 @@
+const Array = [10,20,30];
+//Array[0] = (Array[0]+3);
+//Array[1] = (Array[1]+3);
+//Array[2] = (Array[2]+3);
+//console.log(Array);
+
+Array.array.forEach(Number => {
+    return Number + 3
+});
+
+console.log(Array);
